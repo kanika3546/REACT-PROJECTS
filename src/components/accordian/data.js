5af666fcd782387a07fe7b3a7e3d2909a3712706
kanika3546 <kanika3546@gmail.com>
2025-02-05 17:37:20 +0530

    accordian completed

---
+++ src/components/accordian/data.js
@@ -0,0 +1,25 @@
+const data= [
+{
+    id: '1',
+    question: "What is accordian?",
+    answer: "Acrordian are componenets used in user interface"
+},
+{
+    id: '2',
+    question: "What is accordian2?",
+    answer: "Acrordian are componenets used in user interface"
+},
+{
+    id: '3',
+    question: "What is accordian3?",
+    answer: "Acrordian are componenets used in user interface"
+},
+{
+    id: '4',
+    question: "What is accordian4?",
+    answer: "Acrordian are componenets used in user interface"
+},
+
+];
+
+export default data;
