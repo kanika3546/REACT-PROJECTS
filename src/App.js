57f7f04fc05eadd9630f340c798f8dc54d550bc5
kanika3546 <kanika3546@gmail.com>
2025-02-10 21:03:23 +0530

    tree-view/ recurvise menu

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import ImageSlider from './components/image-slider';
 import LoadMoreData from './components/load-more-data';
 import RandomColor from './components/random-color';
 import StarRating from './components/star-rating';
-
+import TreeView from './components/Tree-view';
+import menus from './components/Tree-view/data';
 function App() {
   return (
     <div className="App">
-    {/* {Accordian} */}
+   
 {/* <Accordian /> */}
 
 {/* <RandomColor /> */}
@@ -19,7 +20,11 @@ function App() {
 {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/> */}
 
 
-<LoadMoreData />
+{/* <LoadMoreData /> */}
+
+
+<TreeView menus={menus}/>
+
     </div>
   );
 }
